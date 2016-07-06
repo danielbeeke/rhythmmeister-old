3ffb5176f7a2e325918d4dd8e9640e4ed92e6afb
Daniel Beeke <daniel@studiofonkel.nl>
2016-07-06 11:46:10 +0200

    Adding a couple of comments with font variants so I can write the formula

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    $(document).bind('keydown', 'ctrl+\\', function () {
+        toggleBodyClass('has-visible-vertical-rhythm-grid');
+    });
+
+    $(document).bind('keydown', "ctrl+'", function () {
+        toggleBodyClass('has-visible-vertical-rhythm-helpers');
+    });
+
+    $(document).bind('keydown', "ctrl+;", function () {
+        toggleBodyClass('has-visible-horizontal-grid');
+    });
+
+    function toggleBodyClass(className) {
+        var activeClasses = JSON.parse(localStorage.getItem('activeClasses'));
+        if (!activeClasses) {
+            activeClasses = {}
+        }
+        $('body').toggleClass(className);
+        activeClasses[className] = $('body').hasClass(className);
+        localStorage.setItem('activeClasses', JSON.stringify(activeClasses));
+    }
+
+    var activeClasses = JSON.parse(localStorage.getItem('activeClasses'));
+    $.each(activeClasses, function (className, value) {
+        if (value) {
+            $('body').addClass(className);
+        }
+    })
+});
